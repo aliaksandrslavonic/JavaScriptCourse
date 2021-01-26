9884cf25541bb5e954033815fc22ac1af5b703b1
Aleksandr Bylina <aleksandr.bylina@gismart.com>
2021-01-26 20:09:14 +0300

    fix task6

--- lesson3/task6.js
+++ lesson3/task6.js
@@ -1,13 +1,20 @@
 'use strict';
 
-let somestring = "xxxxxxxxxxxxxxxxxxxx"
-for (var i = 21; i > 1; i--) {
+//вариант 1
+console.log("вариант 1"); 
+
+let somestring = "xxxxxxxxxxxxxxxxxxxx" 
+for (var i = 20; i >= 0; i--) {
     console.log(somestring.substr(i));    
 };
 
-
-
-
+//вариант 2
+console.log("вариант 2"); 
+let somestring2 = ''
+for (var i = 1; i <= 20; i++) {
+    somestring2 += 'x'
+    console.log(somestring2);    
+};
 
 
 
